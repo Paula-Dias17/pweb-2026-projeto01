6ad9ccc07611c27861f3ca67301437c12b6ae16e
Paula-Dias17 <m.paula3405@gmail.com>
2026-05-23 15:39:21 -0300

    inicio do estilo da pagina

---
+++ app/static/js/script.js
@@ -0,0 +1,8 @@
+const links = document.querySelectorAll('.nav-link');
+const currentPage = window.location.pathname.split("/").pop();
+
+links.forEach(link => {
+if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+}
+});
