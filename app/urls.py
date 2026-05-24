d334e191d6fecc708cb474e4d0cb77c604a4c671
Paula-Dias17 <m.paula3405@gmail.com>
2026-05-24 12:38:08 -0300

    corriji erro na url, conteudo dos templates funcionando

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.base, name="base.html"),
     path("inicio", views.index, name="index.html"),
-    path("elenco", views.index, name="elenco.html"),
-    path("sobre", views.index, name="sobre.html"),
+    path("elenco", views.elenco, name="elenco.html"),
+    path("sobre", views.sobre, name="sobre.html"),
 ]
