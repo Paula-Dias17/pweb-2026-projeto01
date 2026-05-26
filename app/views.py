f803bddf5c58677e49f32c70fd045d62ae8b0903
Paula-Dias17 <m.paula3405@gmail.com>
2026-05-26 20:02:21 -0300

    fix cards, informacoes nas views

--- app/views.py
+++ app/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
+    dados_usuario = {"nome": "Michael Douglas", "idade": 23}
+    return render(request, "index.html", dados_usuario)
 
 def sobre(request):
     return render(request, "sobre.html")
 
 def elenco(request):
-    return render(request, "elenco.html")
-
-
-
-# Create your views here.
+    lista_pokemons = [
+        {
+            "nome": "Charizard",
+            "idade": 0,
+            "tipo": "fogo",
+            "imagem": "/static/app/imgs/charizard.png"
+        },
+        {
+            "nome": "Charizard",
+            "idade": 0,
+            "tipo": "fogo",
+            "imagem": "/static/app/imgs/charizard.png"
+        }
+    ]
+        
+    context = {"pokedex": lista_pokemons}
+    return render(request, "elenco.html", context)
